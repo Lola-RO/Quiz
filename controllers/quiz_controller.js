cb584eb66c3f57ad195203bbebb3ac21b2c0bd3a
Lola Rodríguez <mdrodriguezorviz@gmail.com>
2015-07-25 13:21:42 +0200

    Indice Temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -26,7 +26,7 @@ exports.index = function(req, res){
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', {quiz: req.quiz, errors: []});
+	res.render('quizes/show', {quiz: req.quiz, tema: req.quiz.tema,  errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -42,7 +42,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res){
    var quiz = models.Quiz.build( // crea objeto quiz
-   { pregunta: "Pregunta", respuesta: "Respuesta" }
+   { pregunta: "Pregunta", respuesta: "Respuesta" , tema: "Tema"}
    );
    res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -60,7 +60,7 @@ exports.create = function(req, res){
         } else {
    		// guarda en DB los campos pregunta y respuesta en quiz
    		quiz
-		.save({fields: ["pregunta", "respuesta"]})
+		.save({fields: ["pregunta", "respuesta", "tema"]})
 	        .then(function(){res.redirect('/quizes')});
        }   // Redirection HTTP (URL relativo) lista de preguntas
     }
@@ -78,6 +78,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
    req.quiz.pregunta = req.body.quiz.pregunta;
    req.quiz.respuesta = req.body.quiz.respuesta;
+   req.quiz.tema = req.body.quiz.tema;
 
    req.quiz
    .validate()
@@ -87,7 +88,7 @@ exports.update = function(req, res){
 	    res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
          } else {
             req.quiz // save: guarda campos pregunta y respuesta en DB
-            .save( {fields: ["pregunta", "respuesta"]})
+            .save( {fields: ["pregunta", "respuesta", "tema"]})
             .then( function(){ res.redirect('/quizes');});
          } // Redirección HTTP a lista de preguntas (URL relativo)
       }
