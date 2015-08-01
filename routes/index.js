34f5d3c2ba99d841b43c40c27797e618051fea1c
Lola Rodríguez <mdrodriguezorviz@gmail.com>
2015-08-01 17:19:13 +0200

    Crear comentario

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
-// Página de entrada ( home page
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
@@ -22,5 +23,7 @@ router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',		quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
 
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 module.exports = router;
